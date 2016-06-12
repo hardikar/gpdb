f2aab68d3d944b8b83d4e8bf2db97f1807f38390
Shreedhar Hardikar <shardikar@pivotal.io>
2016-06-12 01:10:28 -0700

    download RPMs

--- concourse/scripts/builds/GporcaCodegen.py
+++ concourse/scripts/builds/GporcaCodegen.py
@@ -4,7 +4,30 @@ import sys
 
 from GporcaCommon import GporcaCommon
 
+LLVM_RPM_URL="https://github.com/hardikar/rpm-specs/releases/download/centos6/llvm37-3.7.1-4.el6.x86_64.rpm"
+CLANG_RPM_URL="https://github.com/hardikar/rpm-specs/releases/download/centos6/clang-3.7.1-4.el6.x86_64.rpm"
+
 class GporcaCodegen(GporcaCommon):
+    def _download_and_install_rpm(self, url):
+        status = subprocess.call(["curl", url, "-o", "t.rpm"])
+        if status:
+            return status
+        status = subprocess.call(["rpm", "-i", "t.rpm"])
+        if status:
+            return status
+
+    def install_system_deps(self):
+        status = GporcaCommon.install_system_deps(self)
+        if status:
+            return status
+        status = self._download_and_install_rpm(LLVM_RPM_URL)
+        if status:
+            return status
+        status = self._download_and_install_rpm(CLANG_RPM_URL)
+        if status:
+            return status
+        
+
     def configure(self):
         return subprocess.call(["./configure",
                                 "--enable-orca",
