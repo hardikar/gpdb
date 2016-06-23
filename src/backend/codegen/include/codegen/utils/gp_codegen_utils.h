163471c76b27131a0ac97670e19b3094987e6a62
Shreedhar Hardikar <shardikar@pivotal.io>
2016-06-23 16:05:28 -0700

    Add gp_codegen_utils header and C file

---
+++ src/backend/codegen/include/codegen/utils/gp_codegen_utils.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+//  Greenplum Database
+//  Copyright 2016 Pivotal Software, Inc.
+//
+//  @filename:
+//    codegen_utils.h
+//
+//  @doc:
+//    Object that manages runtime code generation for a single LLVM module.
+//
+//  @test:
+//
+//
+//---------------------------------------------------------------------------
+
+#ifndef GPCODEGEN_GP_CODEGEN_UTILS_H_  // NOLINT(build/header_guard)
+#define GPCODEGEN_GP_CODEGEN_UTILS_H_
+
+namespace gpcodegen {
+}  // namespace gpcodegen
+
+#endif  // GPCODEGEN_GP_CODEGEN_UTILS_H_
+// EOF
---
+++ src/backend/codegen/utils/gp_codegen_utils.cc
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+//  Greenplum Database
+//  Copyright 2016 Pivotal Software, Inc.
+//
+//  @filename:
+//    codegen_utils.cc
+//
+//  @doc:
+//    Object that manages runtime code generation for a single LLVM module.
+//
+//  @test:
+//    Unittests in tests/code_generator_unittest.cc
+//
+//
+//---------------------------------------------------------------------------
+
+#include "codegen/utils/gp_codegen_utils.h"
+
+namespace gpcodegen {
+}  // namespace gpcodegen
+
+// EOF
