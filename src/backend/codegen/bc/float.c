88176ed68ae8eaf3ed59ad64b0bf446cb84ea7e9
Shreedhar Hardikar <shardikar@pivotal.io>
2016-08-04 15:36:10 -0700

    Magic in float.c

--- src/backend/codegen/bc/float.c
+++ src/backend/codegen/bc/float.c
@@ -1,5 +1,7 @@
 #include <math.h>
 
+#define true 1
+#define false 0
 
 #define CHECKFLOATVAL(val, inf_is_valid, zero_is_valid)			\
 do {															\
@@ -9,15 +11,169 @@ do {															\
 		("value out of range: underflow");				\
 } while(0)
 
-float
-float8mul(float arg1, float arg2)
+typedef long int Datum;
+typedef float float8;
+
+#define PG_RETURN_FLOAT8(result) return (Datum) result
+#define PG_RETURN_BOOL(result) return (Datum) result
+#define PG_RETURN_INT32(result) return (Datum) result
+
+#define PG_FUNCTION_ARGS Datum in_arg_0, Datum in_arg_1
+#define PG_GETARG_FLOAT8(i) ((float8) in_arg_##i)
+
+
+Datum
+float8pl(PG_FUNCTION_ARGS)
 {
-	float		result;
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+  float8    result;
 
-	result = arg1 * arg2;
+  result = arg1 + arg2;
 
-	CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
-				  arg1 == 0 || arg2 == 0);
-	return result;
+  CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+  PG_RETURN_FLOAT8(result);
 }
 
+Datum
+float8mi(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+  float8    result;
+
+  result = arg1 - arg2;
+
+  CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), true);
+  PG_RETURN_FLOAT8(result);
+}
+
+Datum
+float8mul(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+  float8    result;
+
+  result = arg1 * arg2;
+
+  CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2),
+          arg1 == 0 || arg2 == 0);
+  PG_RETURN_FLOAT8(result);
+}
+
+Datum
+float8div(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+  float8    result;
+
+//  if (arg2 == 0.0)
+//    ereport(ERROR,
+//        (errcode(ERRCODE_DIVISION_BY_ZERO),
+//         errmsg("division by zero")));
+
+  result = arg1 / arg2;
+
+  CHECKFLOATVAL(result, isinf(arg1) || isinf(arg2), arg1 == 0);
+  PG_RETURN_FLOAT8(result);
+}
+
+
+/*
+ *    float8{eq,ne,lt,le,gt,ge}   - float8/float8 comparison operations
+ */
+static int
+float8_cmp_internal(float8 a, float8 b)
+{
+  /*
+   * We consider all NANs to be equal and larger than any non-NAN. This is
+   * somewhat arbitrary; the important thing is to have a consistent sort
+   * order.
+   */
+  if (isnan(a))
+  {
+    if (isnan(b))
+      return 0;     /* NAN = NAN */
+    else
+      return 1;     /* NAN > non-NAN */
+  }
+  else if (isnan(b))
+  {
+    return -1;        /* non-NAN < NAN */
+  }
+  else
+  {
+    if (a > b)
+      return 1;
+    else if (a < b)
+      return -1;
+    else
+      return 0;
+  }
+}
+
+
+Datum
+float8eq(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) == 0);
+}
+
+Datum
+float8ne(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) != 0);
+}
+
+Datum
+float8lt(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) < 0);
+}
+
+Datum
+float8le(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) <= 0);
+}
+
+Datum
+float8gt(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) > 0);
+}
+
+Datum
+float8ge(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_BOOL(float8_cmp_internal(arg1, arg2) >= 0);
+}
+
+Datum
+btfloat8cmp(PG_FUNCTION_ARGS)
+{
+  float8    arg1 = PG_GETARG_FLOAT8(0);
+  float8    arg2 = PG_GETARG_FLOAT8(1);
+
+  PG_RETURN_INT32(float8_cmp_internal(arg1, arg2));
+}
