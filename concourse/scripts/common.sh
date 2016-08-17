6fbad5b673ce838a2893bc6b1b41ee3a36d60efe
Shreedhar Hardikar <shardikar@pivotal.io>
2016-08-16 18:35:14 -0700

    Performance pipeline: provision and destroy VM in AWS
    [#130552319]
      * Automate VM provisioning with given VM size
      * Output the VM information, e.g. hostname, IP etc.
      * Be able to SSH to that VM from Pipeline.
      * Destroy VM.
    
    Signed-off-by: Nikos Armenatzoglou <nikos.armenatzoglou@gmail.com>

---
+++ concourse/scripts/common.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+log() {
+  echo -e "$@"
+}
+
+remote_run() {
+  local IP="$1"
+  local KEYFILE="${AWS_KEYPAIR}.pem"
+
+  ssh -i "${KEYFILE}" -t -t ${SSH_USER}@${IP} "${@:2}"
+}
+
+error() {
+  echo >&2 "$@"
+  exit 1
+}
+
+check_config() {
+  # Error out when secret keys are not set
+  if [[ -z $AWS_ACCESS_KEY ]]; then
+      error " AWS_ACCESS_KEY must be specified."
+  fi
+  if [[ -z $AWS_SECRET_KEY ]]; then
+      error "AWS_SECRET_KEY must be specified."
+  fi
+  if [[ -z $AWS_KEYPAIR ]]; then
+      error "AWS_KEYPAIR must be specified."
+  fi
+  if [[ -z $AWS_KEYPAIR_VALUE ]]; then
+      error "AWS_KEYPAIR_VALUE must be specified."
+  fi
+
+  log "Configuration"
+  log "=============================="
+
+  # Display values for non-secret configuration values
+  if [[ -z $AWS_REGION ]]; then
+      error "AWS_REGION must be specified."
+  else
+      log "AWS_REGION=${AWS_REGION}"
+      export EC2_URL="https://ec2.${AWS_REGION}.amazonaws.com"
+      log "EC2_URL=${EC2_URL}"
+  fi
+  if [[ -z $AMI ]]; then
+      error "AMI must be specified."
+  else
+      log "AMI=${AMI}"
+  fi
+  if [[ -z $INSTANCE_TYPE ]]; then
+      error "INSTANCE_TYPE must be specified."
+  else
+      log "INSTANCE_TYPE=${INSTANCE_TYPE}"
+  fi
+  if [[ -z $INSTANCE_NAME ]]; then
+      error "INSTANCE_NAME must be specified."
+  else
+      log "INSTANCE_NAME=${INSTANCE_NAME}"
+  fi
+  if [[ -z $SUBNET_ID ]]; then
+      error "SUBNET_ID must be specified."
+  else
+      log "SUBNET_ID=${SUBNET_ID}"
+  fi
+  if [[ -z $SECURITY_GROUP_ID ]]; then
+      error "SECURITY_GROUP_ID must be specified."
+  else
+      log "SECURITY_GROUP_ID=${SECURITY_GROUP_ID}"
+  fi
+  if [[ -z $SSH_USER ]]; then
+      error "SSH_USER must be specified."
+  else
+      log "SSH_USER=${SSH_USER}"
+  fi
+  if [[ -z $TENANCY ]]; then
+      error "TENANCY must be specified."
+  else
+      log "TENANCY=${TENANCY}"
+  fi
+  if [[ -z $WAIT_SECS ]]; then
+      error "WAIT_SECS must be specified."
+  else
+      log "WAIT_SECS=${WAIT_SECS}"
+      export WAIT=${WAIT_SECS}
+  fi
+  if [[ -z $RETRIES ]]; then
+      error "RETRIES must be specified."
+  else
+      log "RETRIES=${RETRIES}"
+  fi
+
+  log "=============================="
+}
+
+wait_until_status() {
+  local STATUS=$1
+
+  log "Waiting for status: ${STATUS}"
+
+  local N=0
+  until [[ $N -ge $RETRIES ]]; do
+    local COUNT=$(
+      ec2-describe-instances --show-empty-fields ${INSTANCE_IDS[*]} |
+      grep INSTANCE |
+      cut -f6 |
+      grep -c ${STATUS}
+    ) || true
+
+    log "${COUNT} of ${#INSTANCE_IDS[@]} instances ${STATUS}"
+
+    [[ "$COUNT" -eq "${#INSTANCE_IDS[@]}" ]] && break
+
+    N=$(($N+1))
+    sleep $WAIT
+  done
+
+  if [[ $N -ge $RETRIES ]]; then
+    error "Timed out waiting for instances to reach status: ${STATUS}"
+  fi
+}
+
+wait_until_check_ok() {
+  local STATUS=$1
+
+  log "Waiting for instances to pass status checks"
+
+  local N=0
+  until [[ $N -ge $RETRIES ]]; do
+    local COUNT
+
+    COUNT=$(
+      ec2-describe-instance-status --show-empty-fields ${INSTANCE_IDS[*]} |
+      grep -e "\bINSTANCE\b" |
+      cut -f6,7 |
+      xargs -n 2 |
+      grep -c "ok ok"
+    ) || true
+
+    log "${COUNT} of ${#INSTANCE_IDS[@]} instances pass status checks"
+
+    [[ "$COUNT" -eq "${#INSTANCE_IDS[@]}" ]] && break
+
+    COUNT=$(
+      ec2-describe-instance-status --show-empty-fields ${INSTANCE_IDS[*]} |
+      grep -e "\bINSTANCE\b" |
+      cut -f6,7 |
+      xargs -n 2 |
+      grep -c "impaired"
+    ) || true
+
+    if [[ "$COUNT" -gt 0 ]]; then
+      error "${COUNT} of ${#INSTANCE_IDS[@]} failed to pass status checks"
+    fi
+
+    N=$(($N+1))
+    sleep $WAIT
+  done
+
+  if [[ $N -ge $RETRIES ]]; then
+    error "Timed out waiting for instances to pass status checks"
+  fi
+}
---
+++ concourse/scripts/unprovision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set +x
+
+source $(dirname "$0")/common.sh
+
+main() {
+  INSTANCE_IDS=($(cat instance_ids/instance_ids.txt))
+
+  check_config
+
+  log "Deleting instaces : ${INSTANCE_IDS}"
+  ec2-terminate-instances ${INSTANCE_IDS}
+
+  wait_until_status "terminated"
+  log "Instances terminated successfully"
+}
+
+main "$@"
