d3d9f02132736909b7d6edc1b2285086508e7771
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2019-10-18 12:46:08 -0500

    Capture hash opfamily of CMDScalarOp using gpdb::GetCompatibleHashOpFamily()

--- src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
+++ src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
@@ -1791,6 +1791,8 @@ CTranslatorRelcacheToDXL::RetrieveScOp
 
 	BOOL returns_null_on_null_input = gpdb::IsOpStrict(op_oid);
 
+	CMDIdGPDB *mdid_hash_opfamily = GPOS_NEW(mp) CMDIdGPDB(gpdb::GetCompatibleHashOpFamily(op_oid));
+
 	mdid->AddRef();
 	CMDScalarOpGPDB *md_scalar_op = GPOS_NEW(mp) CMDScalarOpGPDB
 											(
@@ -1805,7 +1807,8 @@ CTranslatorRelcacheToDXL::RetrieveScOp
 											m_mdid_inverse_opr,
 											cmp_type,
 											returns_null_on_null_input,
-											RetrieveScOpOpFamilies(mp, mdid)
+											RetrieveScOpOpFamilies(mp, mdid),
+											mdid_hash_opfamily
 											);
 	return md_scalar_op;
 }
