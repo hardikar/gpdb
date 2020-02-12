6cf251215f555e021257a06406ada09a62b6fca3
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2020-02-12 16:41:45 -0600

    Handle config.h.in

--- src/backend/gporca/libgpos/include/gpos/config.h
+++ src/backend/gporca/libgpos/include/gpos/config.h
@@ -13,8 +13,22 @@
 #ifndef GPOS_config_H
 #define GPOS_config_H
 
-#cmakedefine GPOS_DEBUG
+#include "pg_config.h"
 
+/* Get this working for now. */
+/* Idealy we should generate this files just like pg_config.h is generated */
+
+#ifdef USE_ASSERT_CHECKING
+#define GPOS_DEBUG
+#else
+#undef GPOS_DEBUG
+#endif
+
+#define GPOS_x86_64 1
+#define GPOS_64BIT 1
+#define GPOS_Darwin
+
+/*
 // These come from CMAKE_SYSTEM_PROCESSOR
 #cmakedefine GPOS_i386 1
 #cmakedefine GPOS_i686 1
@@ -35,6 +49,7 @@
 #cmakedefine GPOS_GCC_FETCH_ADD_64
 #cmakedefine GPOS_GCC_CAS_32
 #cmakedefine GPOS_GCC_CAS_64
+*/
 
 #endif  // GPOS_config_H
 // EOF
