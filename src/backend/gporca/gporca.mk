c22b8d10939697b5b9dbf29bc7af74026468fef2
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2020-02-13 17:42:13 -0600

    Update Makefiles in src/backend/gpopt

--- src/backend/gporca/gporca.mk
+++ src/backend/gporca/gporca.mk
@@ -1,4 +1,4 @@
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpos/include $(CPPFLAGS)
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpopt/include $(CPPFLAGS)
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libnaucrates/include $(CPPFLAGS)
-override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libcost/include $(CPPFLAGS)
+override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
