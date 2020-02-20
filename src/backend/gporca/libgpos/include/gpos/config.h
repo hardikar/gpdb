426f58a225680bd1f7c45ae3dbba3975a9f7a079
Chris Hajas <chajas@pivotal.io>
2020-02-19 16:55:56 -0800

    Remove 32 bit specific flags

--- src/backend/gporca/libgpos/include/gpos/config.h
+++ src/backend/gporca/libgpos/include/gpos/config.h
@@ -13,8 +13,6 @@
 #ifndef GPOS_config_H
 #define GPOS_config_H
 
-//#include "pg_config.h"
-
 /* Get this working for now. */
 /* Idealy we should generate this files just like pg_config.h is generated */
 
@@ -24,32 +22,7 @@
 #undef GPOS_DEBUG
 #endif
 
-#define GPOS_x86_64 1
-#define GPOS_64BIT 1
 #define GPOS_Darwin
-
-/*
-// These come from CMAKE_SYSTEM_PROCESSOR
-#cmakedefine GPOS_i386 1
-#cmakedefine GPOS_i686 1
-#cmakedefine GPOS_x86_64 1
-#cmakedefine GPOS_sparc 1
-
-// These come from CMAKE_ARCH_BITS (based on CMAKE_SIZEOF_VOID_P)
-#cmakedefine GPOS_32BIT 1
-#cmakedefine GPOS_64BIT 1
-
-// These come from CMAKE_SYSTEM_NAME
-#cmakedefine GPOS_Linux
-#cmakedefine GPOS_Darwin
-#cmakedefine GPOS_SunOS
-
-// Atomics, detected by cmake/FindAtomics.cmake
-#cmakedefine GPOS_GCC_FETCH_ADD_32
-#cmakedefine GPOS_GCC_FETCH_ADD_64
-#cmakedefine GPOS_GCC_CAS_32
-#cmakedefine GPOS_GCC_CAS_64
-*/
 
 #endif  // GPOS_config_H
 // EOF
